8e2dd342069d4da02a07156f6ea7f789de41bf27
Matt Papais <mapapais@oakland.edu>
2016-02-04 10:51:11 -0500

    Added the functionality to the sing the song

---
+++ Main.java
@@ -0,0 +1,8 @@
+public class Main {
+
+    public static void main(String[] args) {
+        Ninety_Nine_Bottles bottles = new Ninety_Nine_Bottles();
+        bottles.bottles_on_wall();
+    }
+
+}
---
+++ Ninety_Nine_Bottles.java
@@ -0,0 +1,19 @@
+/**
+ * Created by Matt on 2/4/2016.
+ */
+public class Ninety_Nine_Bottles {
+    public static void bottles_on_wall(){
+        for(int i = 99; i > 0; i--)
+        {
+            int bottles = (i - 1);
+            String word = " bottles";
+            if(i == 1 || bottles == 1){
+                word = " bottle";
+            }
+            System.out.println(i + word + " of beer on the wall, " + i + word + " of beer");
+            System.out.println("take one down, pass it around, " + bottles + word + " of beer on the wall");
+
+        }
+        System.out.println("No more bottles of beer on the wall.");
+    }
+}
